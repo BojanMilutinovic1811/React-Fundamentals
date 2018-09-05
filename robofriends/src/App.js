493f3dfbbdac0db97b1a97a02be9b67f7e18513a
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-09-05 23:12:57 +0200

    update

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import {robots} from './robots.js';
 import Cardlist from './Cardlist.js';
 import Searchfield from './Searchfield';
+import Scroll from './Scroll';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       Searchfield: ''
     }
   }
@@ -17,6 +17,12 @@ class App extends Component {
   }
 
 
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response=>response.json())
+  .then( users => this.setState({robots: users }))
+}
+
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLocaleLowerCase().includes(this.state.Searchfield.toLocaleLowerCase())
@@ -25,8 +31,9 @@ class App extends Component {
       <div className='tc'>
         <h1 className='robotitle'>ROBOFRIENDS</h1>
         <Searchfield onSearchChange={this.onSearchChange}/>
+        <Scroll>
         <Cardlist robots={filteredRobots}/>
-        
+        </Scroll>
       </div>
     );
   }
