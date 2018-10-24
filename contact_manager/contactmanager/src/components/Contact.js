59b69d9b816f36d5e2197253b54c8dffd16d0859
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-10-24 15:19:03 +0200

    context

--- contact_manager/contactmanager/src/components/Contact.js
+++ contact_manager/contactmanager/src/components/Contact.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {Consumer} from '../Context';
 
 class Contact extends Component {
   state = {
     showContactInfo: false
   }
   
+  onDeleteClick = (id, dispatch) => {
+    console.log('hello  ')
+    dispatch({type:'DELETE_CONTACT', payload: id})  
+  }
 
   render() {
-    const {name, email, phone} = this.props.contact;
+    const {id, name, email, phone} = this.props.contact;
     const {showContactInfo} = this.state;
-    console.log(showContactInfo)
+    
   return (
-      <div className='card card-body mb-3'>
-        <h4>{name} <i onClick={ () => this.setState({showContactInfo:!this.state.showContactInfo})} className="fas fa-sort-down"></i></h4>
-        {showContactInfo ? (<ul className='list-group'>
-            <li className='list-group-item'>email: {email}</li>
-            <li className='list-group-item'>phone: {phone}</li>
-        </ul>) : null }
-        
-      </div>
+    <Consumer>
+      {value => {
+        const {dispatch} = value;
+        return(
+          <div className='card card-body mb-3'>
+              <h4>{name} 
+              <i onClick={ () => this.setState({showContactInfo:!this.state.showContactInfo})} className="fas fa-sort-down" style={{cursor:'pointer'}}>
+              </i>
+              <i className='fas fa-times' onClick={this.onDeleteClick.bind(this, id, dispatch)} style={{cursor:'pointer', float:'right', color: 'red'}}>
+              </i>
+              </h4>
+              {showContactInfo ? (<ul className='list-group'>
+                  <li className='list-group-item'>email: {email}</li>
+                  <li className='list-group-item'>phone: {phone}</li>
+              </ul>) : null }
+              
+            </div>
+        )
+      }}
+    </Consumer>
+      
     )
   }
 }
--- contact_manager/contactmanager/src/components/Contacts.js
+++ contact_manager/contactmanager/src/components/Contacts.js
@@ -1,40 +1,24 @@
 import React, { Component } from 'react';
-import Contact from './Contact'
+import Contact from './Contact';
+import {Consumer} from '../Context'
 
 class Contacts extends Component {
     
-    state = {
-            contacts: [
-                {
-                    id: 1,
-                    name: 'John Doe',
-                    email: 'jdoe@gmail.com',
-                    phone: '222-222-2222'
-                },
-                {
-                    id: 2,
-                    name: 'Bob Rock',
-                    email: 'bobby@gmail.com',
-                    phone: '222-333-2222'
-                },
-                {
-                    id: 3,
-                    name: 'Alan Ford',
-                    email: 'fordy@gmail.com',
-                    phone: '222-111-2222'
-                }]
-        } 
-    
-  
     render() {
-     const {contacts} = this.state;
-    return (
-      <div>
-        {contacts.map(contact => 
-            <Contact contact={contact} key={contact.id}/>
-        )}
-      </div>
-    )
+        return (
+            <Consumer>  
+                {value => {
+                    const {contacts} = value;
+                    return(
+                        <React.Fragment>
+                            {contacts.map(contact => 
+                            (<Contact contact={contact} key={contact.id}/>
+                            ))}
+                        </React.Fragment>
+                    )
+                }}
+            </Consumer>
+        )
   }
 }
 
