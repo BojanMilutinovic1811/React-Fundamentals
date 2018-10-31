56b013f367c93125c045e15ae329709ee1774e8d
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-10-31 21:37:44 +0100

    client panel initial commit

---
+++ client_panel/src/components/layout/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Clients from '../clients/clients'
+
+const Dashboard = () => {
+  return (
+    <div className='row'>
+      <div className='col-md-10'>
+      <Clients/>
+      </div>
+      <div className='col-md-2'>
+      <Sidebar/>
+      </div>
+    </div>
+  )
+}
+
+
+export default Dashboard;
